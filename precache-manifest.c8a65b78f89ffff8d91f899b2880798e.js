8d509c60c2d4cf7a2ba6fed14fa7a13aad18cc80
gassde <gassde@ecs.csus.edu>
2019-11-09 19:28:52 -0800

    Updates

---
+++ precache-manifest.c8a65b78f89ffff8d91f899b2880798e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50eef7f4c02d189d83e8f2122d8e55f",
+    "url": "/connect-to-social/index.html"
+  },
+  {
+    "revision": "bdcb47c31790912c5a71",
+    "url": "/connect-to-social/static/css/main.b0992723.chunk.css"
+  },
+  {
+    "revision": "0aec9bb87f6943e0c2de",
+    "url": "/connect-to-social/static/js/2.412e7f8e.chunk.js"
+  },
+  {
+    "revision": "bdcb47c31790912c5a71",
+    "url": "/connect-to-social/static/js/main.038967ad.chunk.js"
+  },
+  {
+    "revision": "8871e56a0aaefff09ec3",
+    "url": "/connect-to-social/static/js/runtime-main.7a665ed0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/connect-to-social/static/media/logo.25bf045c.svg"
+  }
+]);
