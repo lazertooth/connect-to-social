9b6a6616864cb72b4ac1d28b71b8ed7c8dca5036
gassde <gassde@ecs.csus.edu>
2019-11-09 22:05:43 -0800

    soundcloud embed?

--- connect/src/App.js
+++ connect/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Connect from "./Connect";
+import ReactSoundcloud from 'react-soundcloud-embed';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ReactSoundcloud url="https://soundcloud.com/chill/shrines-spoken-bird-beautiful"/>
         <Connect />
       </header>
     </div>
